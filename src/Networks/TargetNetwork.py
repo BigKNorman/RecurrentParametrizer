c88d36e54ecb6ec4019bee7f36aa7ceada0a0760
norman17 <norman17@purdue.edu>
2023-10-17 21:52:44 -0400

    more work on parametrizer and target network

--- src/Networks/TargetNetwork.py
+++ src/Networks/TargetNetwork.py
@@ -1,7 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 
-
-class TargetNetwork:
+class TargetNetwork(nn.Module):
     def __init__ (self, inputs, outputs, num_params, num_layers=1): 
-        self.network = FF(
+
+        self.layer1 = nn.Linear(inputs, num_params)
+        #self.activation = F.relu
+
+
+    def initHidden(self): 
+        return torch.zeros(1, self.hidden_size)
+
+
+    def forward(self, x): 
+        x = F.relu(self.layer1(x))
+        return x
